1e85b0ceafe62be9e28cdb7eaa820d8a698fe5ab
Lito Enriquez Dagodog <lito@lanexus.com>
2022-01-27 19:44:05 +0800

    add

--- cypress/integration/Stress Testing-MessagesPage.js
+++ cypress/integration/Stress Testing-MessagesPage.js
@@ -14,7 +14,7 @@ Cypress._.times(1,() => {
 			cy.viewport(1280, 720)
 			cy.server()
 			cy.route('POST', '/user/authenticate').as('userLogin')
-			cy.route('GET','/api/v1/client/5be9eaa8ca50da00231cd33d/review/summary?report**').as('filters') //filterXHR result
+			cy.route('GET','/api/v1/client/5e59d44230a5d333f453ef2f/feedback?filters[]=created_at**').as('filters') //filterXHR result
 			cy.route('GET','/api/v1/warehouse-status').as('warehouseStatus')
 			cy.visit('/')
 			.get('#email').type('lito+superadmin@lanexus.com')
@@ -22,16 +22,16 @@ Cypress._.times(1,() => {
 			.get('#signin-btn').click()
 
 		}) 
-		it('Stress Testing on Dashboard Page', function() {
+		it('Account Management Page', function() {
 			cy.wait('@userLogin').should('have.property', 'status', 200)
 			//cy.wait('@accountMgnt').should('have.property', 'status', 200)
 			cy.get('.row > :nth-child(1) > h2').should('contain','Account Management')
 			cy.get('#clients-tab > .search-options-cont > :nth-child(2) > .checkmark').click({force:true})
 			cy.get('#clients-tab > .search-options-cont > :nth-child(3) > .checkmark').click({force:true})
-			cy.get('#clients-tab > .multiselect > .multiselect__tags > .multiselect__input').type('5be9eaa8ca50da00231cd33d') //PROD
+			cy.get('#clients-tab > .multiselect > .multiselect__tags > .multiselect__input').type('5e59d44230a5d333f453ef2f') //PROD
+			//cy.get('#clients-tab > .multiselect > .multiselect__tags > .multiselect__input').type('5bea2b74ca50da00214ea71a') //PROD
 			//cy.get('#clients-tab > .multiselect > .multiselect__tags > .multiselect__input').type('5cd4d7cb2ff63300292edd81') //STG
 			//cy.get('#clients-tab > .multiselect > .multiselect__tags > .multiselect__input').type('5bd7d4d520116100237fcd4c') //QA
-			//cy.wait('@searchByClientID').should('have.property', 'status', 200)
 			cy.get('#clients-tab > .multiselect > .multiselect__tags > .multiselect__input').invoke('val').as('client_id')
 			cy.get('@client_id').then((clientID) => {
 				const client_id = clientID
@@ -41,13 +41,12 @@ Cypress._.times(1,() => {
 			cy.get('.client-tab-list-cont > .text-right > :nth-child(2)').click({force:true})
 			cy.wait('@warehouseStatus').should('have.property', 'status', 200)
 			cy.wait(5000)
-			//filter last 12months
-			cy.get('#period-filter-dropdown > div > a:nth-child(5)').click({force:true})
+			// GO TO MESSAGES PAGE
+			cy.get(':nth-child(5) > ul > :nth-child(5) > .mr-2').click({force:true})
 			cy.wait('@filters').should('have.property', 'status', 200)
-			cy.wait(10000)
-			//SIGN-OUT CLIENT ADMIN
+			//sign-out client admin
 			cy.get('.sign-out-container > a > span').click({force:true})
-			//SIGN-OUT SUPER ADMIN
+			//sign-out super admin
 			cy.wait(5000)
 			cy.get('a > span').click()
 		})
